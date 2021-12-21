cc159c0cbe50046f5b48614b4e66a5076204b480
ajikee1 <akeeri1@students.towson.edu>
2021-12-20 22:30:22 -0500

    first commit

---
+++ src/main/java/api/MockRecruiterApplication.java
@@ -0,0 +1,16 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+
+@SpringBootApplication
+public class MockRecruiterApplication {
+
+    public static void main(String[] args) {
+
+        ConfigurableApplicationContext context =  SpringApplication.run(MockRecruiterApplication.class, args);
+    }
+
+}
---
+++ src/main/java/api/apiRequests.java
@@ -0,0 +1,72 @@
+package api;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pojo.recruit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class apiRequests {
+
+    private List<recruit> recruits = new ArrayList<>();
+
+
+    private void addTestRecruit(){
+        if(recruits.isEmpty()){
+            recruits.add(new recruit("Ajith", "Keerikkattil", "ajith@yopmail.com", "Computer Science"));
+            recruits.add(new recruit("Kari", "Dominguez", "kari@yopmail.com", "Information System"));
+        }
+    }
+
+
+    /* ADD A RECRUIT */
+    @RequestMapping(value="/recruit/", method = RequestMethod.POST)
+    public HttpStatus addRecruit(@RequestBody recruit r){
+        recruits.add(r);
+        ResponseEntity<String> entity = new ResponseEntity<>("RECRUIT ADDED", HttpStatus.CREATED);
+
+        HttpStatus statusCode = entity.getStatusCode();
+        String body = entity.getBody();
+
+        return statusCode;
+    }
+
+
+    /* GET ALL RECRUITS */
+    @RequestMapping(value="/recruits/")
+    public ResponseEntity getRecruits(){
+        addTestRecruit();
+        return new ResponseEntity<>(recruits, HttpStatus.OK);
+    }
+
+
+    /* GET SPECIFIED RECRUITS BY FIRSTNAME*/
+    @RequestMapping(value = "/recruit/{firstName}")
+    public ResponseEntity getRecruit(@PathVariable("firstName") String firstName) {
+
+        for(recruit r: recruits){
+            if(r.getFirstName().equals(firstName)){
+                return new ResponseEntity<>(r, HttpStatus.OK);
+            }
+        }
+
+        return null;
+    }
+
+    /* EMAIL THE RECRUIT */
+    @RequestMapping(value = "/recruit/notify/{firstName}")
+    public String emailRecruit(@PathVariable("firstName") String firstName){
+
+        for(recruit r: recruits){
+            if(r.getFirstName().equals(firstName)){
+                String email = r.getEmail();
+
+                //todo:: code to send email
+            }
+        }
+        return null;
+    }
+}
